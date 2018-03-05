143f5ea12dd485bc8b847ab000e187aee66fd0a8
Andrew <a.massamillo@gmail.com>
2018-03-05 15:05:56 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = props => {
+
+
+  return (
+    <section>
+      <h1>HOME PAGE</h1>
+      </section>
+  )
+}
+
+export default Home
---
+++ src/pages/search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Search = props => {
+
+
+  return (
+    <section>
+      <h1>SEARCH PAGE</h1>
+      </section>
+  )
+}
+
+export default Search
